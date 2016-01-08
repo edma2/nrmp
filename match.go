c481ec2e76591de6c19fa87fc653184ebec16f4a
Eugene Ma <ema@twitter.com>
2016-01-07 21:22:10 -0800

    Initial implementation and unit test

--- match.go
+++ match.go
@@ -2,3 +2,23 @@
 // as described here:
 // http://www.nrmp.org/match-process/match-algorithm/
 package nrmp
+
+type applicantSet map[*Applicant]bool
+
+type Program struct {
+	name  string
+	slots applicantSet
+	ranks []*Applicant
+}
+
+type Applicant struct {
+	name  string
+	ranks []*Program
+}
+
+// Run the match algorithm. After it is finished, program slots
+// will contain the assignments. If an error is encountered, return
+// a non-nil error.
+func Match(apps []*Applicant) error {
+	return nil
+}

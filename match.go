482d806ed778885b995256edabc1ad65238035da
Eugene Ma <ema@twitter.com>
2016-01-07 22:50:21 -0800

    ok

--- match.go
+++ match.go
@@ -3,22 +3,57 @@
 // http://www.nrmp.org/match-process/match-algorithm/
 package nrmp
 
-type applicantSet map[*Applicant]bool
-
 type Program struct {
-	name  string
-	slots applicantSet
-	ranks []*Applicant
+	name    string
+	matches []*Applicant
+	ranking map[*Applicant]int
 }
 
 type Applicant struct {
-	name  string
-	ranks []*Program
+	name    string
+	ranking []*Program
+}
+
+func (a Applicant) String() string {
+	return a.name
+}
+
+func (p Program) String() string {
+	return p.name
+}
+
+func NewProgram(name string, size int, ranking []*Applicant) *Program {
+	p := Program{
+		name:    name,
+		matches: make([]*Applicant, size),
+		ranking: make(map[*Applicant]int),
+	}
+	for i, a := range ranking {
+		p.ranking[a] = i
+	}
+	return &p
+}
+
+// Match an applicant to this program and return true
+// if successful, false otherwise.
+func (p Program) Match(a *Applicant) bool {
+	for i := 0; i < len(p.matches); i++ {
+		if p.matches[i] == nil {
+			p.matches[i] = a
+			return true
+		} else {
+			if p.ranking[a] > p.ranking[p.matches[i]] {
+				p.matches[i] = a
+				return true
+			}
+		}
+	}
+	return false
 }
 
 // Run the match algorithm. After it is finished, program slots
 // will contain the assignments. If an error is encountered, return
 // a non-nil error.
-func Match(apps []*Applicant) error {
+func Match(as []*Applicant) error {
 	return nil
 }
--- match_test.go
+++ match_test.go
@@ -3,56 +3,50 @@ package nrmp
 import "testing"
 
 func TestMatch(t *testing.T) {
-	var pikesville, memorial, albany Program
-	var dawson, chen, perez, patel Applicant
+	var pikesville, memorial, albany *Program
+	var dawson, chen, perez, patel *Applicant
 
-	dawson = Applicant{
-		name:  "Dawson",
-		ranks: []*Program{&memorial, &albany, &pikesville},
+	dawson = &Applicant{
+		name:    "Dawson",
+		ranking: []*Program{memorial, albany, pikesville},
 	}
-	chen = Applicant{
-		name:  "Chen",
-		ranks: []*Program{&pikesville, &albany, &memorial},
+	chen = &Applicant{
+		name:    "Chen",
+		ranking: []*Program{pikesville, albany, memorial},
 	}
-	patel = Applicant{
-		name:  "Patel",
-		ranks: []*Program{&albany},
+	perez = &Applicant{
+		name:    "Perez",
+		ranking: []*Program{pikesville, albany},
+	}
+	patel = &Applicant{
+		name:    "Patel",
+		ranking: []*Program{albany},
 	}
 
-	pikesville = Program{
-		name:  "Pikesville",
-		slots: make(applicantSet, 1),
-		ranks: []*Applicant{&perez, &patel, &chen},
-	}
-	memorial = Program{
-		name:  "Memorial",
-		slots: make(applicantSet, 2),
-		ranks: []*Applicant{&perez, &dawson, &patel},
-	}
-	albany = Program{
-		name:  "Albany",
-		slots: make(applicantSet, 2),
-		ranks: []*Applicant{&chen, &dawson, &perez, &patel},
-	}
+	pikesville = NewProgram("Pikesville", 1, []*Applicant{perez, patel, chen})
+	memorial = NewProgram("Memorial", 2, []*Applicant{perez, dawson, patel})
+	albany = NewProgram("Albany", 2, []*Applicant{chen, dawson, perez, patel})
 
 	expectedMatching := map[*Program][]*Applicant{
-		&pikesville: []*Applicant{&perez},
-		&memorial:   []*Applicant{&dawson},
-		&albany:     []*Applicant{&chen, &patel},
+		pikesville: []*Applicant{perez},
+		memorial:   []*Applicant{dawson},
+		albany:     []*Applicant{chen, patel},
 	}
 
-	err := Match([]*Applicant{&dawson, &chen, &perez, &patel})
+	err := Match([]*Applicant{dawson, chen, perez, patel})
 	if err != nil {
 		t.Error(err)
 	}
 	for prog, apps := range expectedMatching {
-		if len(apps) != len(prog.slots) {
-			t.Errorf("Size of %s was expected %d, but was actually %d!\n", prog.name, len(apps), len(prog.slots))
-		} else {
-			for _, app := range apps {
-				if !prog.slots[app] {
-					t.Errorf("%s did not contain %s!\n", prog.name, app.name)
-				}
+		for i, actual := range prog.matches {
+			var expected *Applicant
+			if i < len(apps) {
+				expected = apps[i]
+			} else {
+				expected = nil
+			}
+			if expected != actual {
+				t.Errorf("%s: expected %s in position %d, but got %s!\n", prog, expected, i, actual)
 			}
 		}
 	}
